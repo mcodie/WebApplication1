85dc5bf80fb779c33157106a56860b1ef174597d
e0056533 <e0056533@LT74CPM32>
2016-06-22 13:24:53 -0500

    bug fix

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page goes here and below (new branch code and also orig).";
+            ViewBag.Message = "Your application description page goes here and below (deleted everything).";
 
             return View();
         }
