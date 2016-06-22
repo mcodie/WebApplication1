1e9ec1d3951356b258d8bf5e3fb74b86e60a52e5
e0056533 <e0056533@LT74CPM32>
2016-06-22 12:55:32 -0500

    check in master branch changes

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page goes here and below.";
+            ViewBag.Message = "Your application description page goes here and below (added to original branch).";
 
             return View();
         }
